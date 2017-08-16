521a91db6667634b119d46c84c362e4f52f159c7
UejdisDushi <20010884@studenti.uniupo.it>
2017-08-16 09:38:47 +0200

    Carrello funziona correttamente

--- GranBazar/Controllers/CarrelloController.cs
+++ GranBazar/Controllers/CarrelloController.cs
@@ -14,6 +14,7 @@ namespace GranBazar.Controllers
 
         BazarContext Context;
         List<Prodotto> prodottiInCarrello;
+        List<int> quantitaPerProdotto;
         public CarrelloController(BazarContext context)
         {
             Context = context;
@@ -23,25 +24,67 @@ namespace GranBazar.Controllers
         //dobbiamo mettere le cose in sessione, così non funziona
         public IActionResult Index(int? id)
         {
-            if (id != null)
+            if (id != null)// se inserisco il primo elemento
             {
+                //dall'id recupero l'elemento
                 var query =
                     from x in Context.Prodotto
                     where x.IdProdotto == id
                     select x;
+       
+                var tempProdottiInCarrello = HttpContext.Session.Get<List<Prodotto>>("prodottiCarrello");
+                HttpContext.Session.Remove("prodottiCarrello");
+                var tempQta = HttpContext.Session.Get<List<int>>("quantitaPerProdotto");
+                HttpContext.Session.Remove("quantitaPerProdotto");
 
-                var temp = HttpContext.Session.Get<List<Prodotto>>("prodottiCarrello");
-
-                if (temp != null)
+                if (tempProdottiInCarrello != null)
                 {
-                    temp.Add(query.First());
-                    HttpContext.Session.Set<List<Prodotto>>("prodottiCarrello", temp);
+                    bool contenuto = false;
+                    int posizione = 0;
+                    int posizioneContenuto = 0;
+                    foreach(var elem in  tempProdottiInCarrello)// è presente e ho l'indice
+                    {
+                        if(elem.IdProdotto == id)
+                        {
+                            contenuto = true;
+                            posizioneContenuto = posizione;
+                            break;
+                        }
+                        posizione++;
+                    }
+
+                    //prima controllare se è presente 
+
+                    //questa parte ricerca se il prodotto è già presente, se si aggiorno quantita con +1
+                   // Prodotto acazzo = (Prodotto)query.First();
+                    if (contenuto == true)
+                    {//1) Controllo se il prodotto è contenuto --> aggiorno quantità
+                        
+                        //int pos = 0;//2) trovo l indice del prodotto
+                        //f//oreach (var x in tempProdottiInCarrello)
+                           // if (x.IdProdotto == id)
+                              //  pos = tempProdottiInCarrello.IndexOf(x);
+                        //3) aggiorno il vettore delle quantità
+                        tempQta[posizioneContenuto] ++;
+                    }
+                    else
+                    {
+                    
+                        tempProdottiInCarrello.Add(query.First());
+                        tempQta.Add(1);
+                    }
+
+                    HttpContext.Session.Set<List<int>>("quantitaPerProdotto", tempQta);
+                    HttpContext.Session.Set<List<Prodotto>>("prodottiCarrello", tempProdottiInCarrello);
                 }
-                else
+                else //1 volta che entro nel carrello
                 {
                     prodottiInCarrello = new List<Prodotto>();
+                    quantitaPerProdotto = new List<int>();
                     prodottiInCarrello.Add(query.First());
+                    quantitaPerProdotto.Add(1);
                     HttpContext.Session.Set<List<Prodotto>>("prodottiCarrello", prodottiInCarrello);
+                    HttpContext.Session.Set<List<int>>("quantitaPerProdotto", quantitaPerProdotto);
                 }
 
             }
@@ -62,17 +105,43 @@ namespace GranBazar.Controllers
 
             var temp = HttpContext.Session.Get<List<Prodotto>>("prodottiCarrello");
             HttpContext.Session.Remove("prodottiCarrello");
+            var tempQta = HttpContext.Session.Get<List<int>>("quantitaPerProdotto");
+            HttpContext.Session.Remove("quantitaPerProdotto");
+
             int pos = 0;
             foreach (var x in temp)
                 if (x.IdProdotto == id)
                     pos = temp.IndexOf(x);
 
             temp.RemoveAt(pos);
+            tempQta.RemoveAt(pos);
             HttpContext.Session.Set<List<Prodotto>>("prodottiCarrello", temp);
+            HttpContext.Session.Set<List<int>>("quantitaPerProdotto", tempQta);
             return RedirectToAction("Index");
 
         }
 
+        public IActionResult AggiornaCarrello(int quantita, int idProdotto)
+        {
+            var query =
+                from x in Context.Prodotto
+                where x.IdProdotto == idProdotto
+                select x;
+
+            var tempProdottiInCarrello = HttpContext.Session.Get<List<Prodotto>>("prodottiCarrello");
+
+            var tempQta = HttpContext.Session.Get<List<int>>("quantitaPerProdotto");
+            HttpContext.Session.Remove("quantitaPerProdotto");
+
+            int pos = 0;
+            foreach (var x in tempProdottiInCarrello)
+                if (x.IdProdotto == idProdotto)
+                    pos = tempProdottiInCarrello.IndexOf(x);
+            tempQta[pos] = quantita;
+
+            HttpContext.Session.Set<List<int>>("quantitaPerProdotto", tempQta);
+            return RedirectToAction("Index");
+        }
 
     }
 }
