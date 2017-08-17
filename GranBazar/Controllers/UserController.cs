ff68a8a9d64338b5e5287eee8880f0f675efb1cb
UejdisDushi <20010884@studenti.uniupo.it>
2017-08-17 17:19:22 +0200

    Non funziona Index di UserController

--- GranBazar/Controllers/UserController.cs
+++ GranBazar/Controllers/UserController.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using GranBazar.Data;
+using Microsoft.AspNetCore.Http;
+using GranBazar.Models;
 
 namespace GranBazar.Controllers
 {
-
+    [Authorize]
     public class UserController : Controller
     {
         BazarContext context;
@@ -19,10 +21,71 @@ namespace GranBazar.Controllers
         }
         public IActionResult Index()
         {
-            var query =
+
+            string utenteLoggato = HttpContext.Session.GetString("utenteLoggato");
+            var elencoOrdini =
                 from x in context.Ordine
-                select x;
-            TempData["numeroOrdini"] = query.Count();
+                join e in context.Ordine_Prodotto 
+                on x.IdOrdine equals e.IdOrdine 
+                join f in context.Prodotto
+                on e.IdProdotto equals f.IdProdotto
+                where x.Email.Equals(utenteLoggato)
+                select new VistaProdottoOrdine{
+                    IdOrdine = e.IdOrdine,
+                    IdProdotto = e.IdProdotto,
+                    NomeProdotto = f.NomeProdotto,
+                    DescrizioneProdotto = f.DescrizioneProdotto,
+                    Quantita = e.Quantita,
+                    LinkImmagine = f.LinkImmagine,
+                    Prezzo = f.Prezzo,
+                    Sconto = f.Sconto,
+                    DataOrdine = x.DataOrdine,
+                    Stato = x.Stato,
+                    DataSpedizione = x.DataSpedizione
+                };
+            /*
+            var listaId = elencoOrdini
+                .Select(x => new { x.IdOrdine })
+                .Distinct();
+
+
+            Dictionary <int, List<VistaProdottoOrdine>> listaProdotti = new Dictionary<int, List<VistaProdottoOrdine>>();
+
+            List<VistaProdottoOrdine> listaTmp = new List<VistaProdottoOrdine>();
+
+            int ordId = 0;
+
+            /*
+            foreach(var x in listaId.Select((value, i) => new { i, value }))
+            {
+                listaTmp = new List<VistaProdottoOrdine>();
+
+                foreach (var y in listaId.Select((value, i) => new { i, value }))
+                {
+                    if(y.value.IdOrdine == x
+                }
+            }
+            /*
+                    for (int i=0 ; i<listaId.Count() ;i++)
+            {
+                listaTmp = new List<VistaProdottoOrdine>();
+                for (int j = 0; j< elencoOrdini.Count();j++)
+                {
+                    if(elencoOrdini.ElementAt(j).IdOrdine == listaId.ElementAt(i).IdOrdine)
+                    {
+                        listaTmp.Add(elencoOrdini.ElementAt(i));
+                        ordId = listaId.ElementAt(j).IdOrdine;
+                    }
+                }
+                listaProdotti.Add(ordId, listaTmp);
+            }
+            */
+
+      
+ 
+            ViewData["elencoOrdini"] = elencoOrdini;
+           // ViewData["numeroOrdini"] = listaId;
+
             return View();
         }
     }
---
+++ GranBazar/Models/VistaProdottoOrdine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GranBazar.Models
+{
+    public class VistaProdottoOrdine
+    {
+        public int IdOrdine { get; set; }
+        public DateTime DataOrdine { get; set; }
+        public string Stato { get; set; }
+        public DateTime? DataSpedizione { get; set; }
+
+        public int Quantita { get; set; }
+
+        public int IdProdotto { get; set; }
+        public string NomeProdotto { get; set; }
+        public string DescrizioneProdotto { get; set; }
+        public decimal Prezzo { get; set; }
+        public byte? Sconto { get; set; }
+        public string LinkImmagine { get; set; }
+    }
+}
