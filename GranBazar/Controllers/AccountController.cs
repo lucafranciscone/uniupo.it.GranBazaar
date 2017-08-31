c9ea8e6905f986102cdb7467f32f3d5f8daef294
UejdisDushi <20010884@studenti.uniupo.it>
2017-08-31 16:48:35 +0200

    1) Eliminato UserController, ora area riservata user sta dentro AccountController 2) Eliminato AdminController, ora le funzionalita di Admin stanno sotto AccountController 3) Sistemati layout di ElencoUtenti, ElencoOrdini (Admin), CatalogoProdotti. Su tutti questi si può filtrare 4) Eliminato CatalogoProdotti da Prodotti, pagina inutile che veniva utilizzata come esempio all'inizio 5) Sistemate le action del menu 6) Creato file script che accumula gli script creati

--- GranBazar/Controllers/AccountController.cs
+++ GranBazar/Controllers/AccountController.cs
@@ -104,6 +104,7 @@ namespace GranBazar.Controllers
                 return View();
             }
         }
+
         public async Task<IActionResult> Logout()
         {
             await signInManager.SignOutAsync();
@@ -111,7 +112,208 @@ namespace GranBazar.Controllers
             return Redirect("~/");
         }
 
-    
+        public IActionResult ElencoUtenti(string email, string ruolo)
+        {
+            if (email != null && ruolo != null && (ruolo.Equals("Admin") || ruolo.Equals("User")))
+            {
+                var utenteDaAggiornare =
+                     from user in context.Utente
+                     where user.Email.Equals(email)
+                     select user;
 
+                Utente u = utenteDaAggiornare.SingleOrDefault();
+                u.Ruolo = ruolo;
+                context.SaveChanges();
+            }
+
+            var utente =
+                  from user in context.Utente
+                  select user;
+            return View(utente.ToList());
+        }
+
+        /*
+        * da sistemare o ottimizzare questo pezzo, successivamente traformare la vista in tabella filtrabile
+        * */
+        public IActionResult CatalogoProdotti(int? idProdotto, bool? stato, string prezzo, int sconto)
+        {
+            //aggiorno il prezzo
+            if (prezzo != null && idProdotto != null && Decimal.Parse(prezzo) > 0)
+            {
+                var prodottoDaAggiornare =
+                    from prod in context.Prodotto
+                    where prod.IdProdotto == idProdotto
+                    select prod;
+
+                Prodotto p = prodottoDaAggiornare.First();
+                p.Prezzo = Decimal.Parse(prezzo);
+                context.SaveChanges();
+            }
+
+
+            //CASO IN CUI DEVO AGGIORNARE
+            if (idProdotto != null && stato != null)
+            {
+                Boolean update = false;
+                if (stato == true)
+                {
+                    update = true;
+                }
+                var prodottoDaAggiornare =
+                    from prod in context.Prodotto
+                    where prod.IdProdotto == idProdotto
+                    select prod;
+
+                Prodotto p = prodottoDaAggiornare.SingleOrDefault();
+                p.Disponibile = update;
+                context.SaveChanges();
+
+            }
+
+            if (sconto != null && sconto > 0 && idProdotto != null)
+            {
+                //recupero il prodotto da aggiornare
+                var prodottoDaAggiornare =
+                    from prod in context.Prodotto
+                    where prod.IdProdotto == idProdotto
+                    select prod;
+                Prodotto p = prodottoDaAggiornare.SingleOrDefault();
+                p.Sconto = (byte)sconto;
+                context.SaveChanges();
+            }
+
+
+            var catalogoProdotti =
+            from prod in context.Prodotto
+            select prod;
+            return View(catalogoProdotti.ToList());
+
+
+        }
+
+        /*
+        * da sistemare o ottimizzare questo pezzo, successivamente traformare la vista in tabella filtrabile
+        * */
+        public IActionResult ElencoOrdini(int? idOrdine, string stato)
+        {
+            if (idOrdine != null && stato != null)
+                if (stato.Equals("Processato"))
+                {
+                    var ordineDaAggiornare =
+                        from ord in context.Ordine
+                        where ord.IdOrdine == idOrdine
+                        select ord;
+
+                    Ordine p = ordineDaAggiornare.First();
+                    p.Stato = stato;
+                    p.DataSpedizione = DateTime.Now;
+                    context.SaveChanges();
+                }
+
+
+            List<VistaProdottoOrdine> vista = new List<VistaProdottoOrdine>();
+            VistaProdottoOrdine tmpAdd;
+            var idProdottiAcquistatiOrdinatiUltimo =
+              from op in context.Ordine_Prodotto
+              join p in context.Prodotto on op.IdProdotto equals p.IdProdotto
+              join o in context.Ordine on op.IdOrdine equals o.IdOrdine
+              select new
+              {
+                  IdOrdine = op.IdOrdine,
+                  DataOrdine = o.DataOrdine,
+                  Stato = o.Stato,//sent processed
+                  DataSpedizione = o.DataSpedizione,
+                  Quantita = op.Quantita,
+                  IdProdotto = p.IdProdotto,
+                  NomeProdotto = p.NomeProdotto,
+                  DescrizioneProdotto = p.DescrizioneProdotto,
+                  Prezzo = p.Prezzo,
+                  Sconto = p.Sconto,
+                  Email = o.Email,
+
+              };
+
+            foreach (var t in idProdottiAcquistatiOrdinatiUltimo)
+            {
+                tmpAdd = new VistaProdottoOrdine();
+                tmpAdd.IdOrdine = t.IdOrdine;
+                tmpAdd.DataOrdine = t.DataOrdine;
+                tmpAdd.DataSpedizione = t.DataSpedizione;
+                tmpAdd.Quantita = t.Quantita;
+                tmpAdd.IdProdotto = t.IdProdotto;
+                tmpAdd.NomeProdotto = t.NomeProdotto;
+                tmpAdd.DescrizioneProdotto = t.DescrizioneProdotto;
+                tmpAdd.Prezzo = t.Prezzo;
+                tmpAdd.Sconto = t.Sconto;
+                tmpAdd.Email = t.Email;
+                tmpAdd.Stato = t.Stato;
+                vista.Add(tmpAdd);
+            }
+            return View(vista);
+
+        }
+
+        public IActionResult AreaRiservataUser()
+        {
+            var utenteLoggato = HttpContext.Session.Get<Utente>("utenteLoggato");
+
+            var elencoOrdini =
+                from x in context.Ordine
+                join e in context.Ordine_Prodotto
+                on x.IdOrdine equals e.IdOrdine
+                join f in context.Prodotto
+                on e.IdProdotto equals f.IdProdotto
+                where x.Email.Equals(utenteLoggato.Email)
+                select new VistaProdottoOrdine
+                {
+                    IdOrdine = e.IdOrdine,
+                    IdProdotto = e.IdProdotto,
+                    NomeProdotto = f.NomeProdotto,
+                    DescrizioneProdotto = f.DescrizioneProdotto,
+                    Quantita = e.Quantita,
+                    LinkImmagine = f.LinkImmagine,
+                    Prezzo = f.Prezzo,
+                    Sconto = f.Sconto,
+                    DataOrdine = x.DataOrdine,
+                    Stato = x.Stato,
+                    DataSpedizione = x.DataSpedizione
+                };
+
+            var elencoId = elencoOrdini
+                .Select(x => new { x.IdOrdine })
+                .Distinct();
+
+            //converto elencoOrdini in lista
+            var elencoOrdiniInFormatoLista = elencoOrdini.ToList();
+
+            //converto listaId in lista
+            var elencoIdInFormatoLista = elencoId.ToList();
+
+
+            Dictionary<int, List<VistaProdottoOrdine>> elencoIdOrdiniConAssociatoElencoProdotti = new Dictionary<int, List<VistaProdottoOrdine>>();
+
+            //dichiaro una lista temporanea che conterrà l'elenco prodotti per un singolo id ordine
+            List<VistaProdottoOrdine> listaTmp = new List<VistaProdottoOrdine>();
+
+            int ordineIdPerHash = 0;
+
+            for (int i = 0; i < elencoIdInFormatoLista.Count(); i++)
+            {
+                listaTmp = new List<VistaProdottoOrdine>();
+                for (int j = 0; j < elencoOrdiniInFormatoLista.Count(); j++)
+                {
+                    if (elencoOrdiniInFormatoLista.ElementAt(j).IdOrdine == elencoIdInFormatoLista.ElementAt(i).IdOrdine)
+                    {
+                        listaTmp.Add(elencoOrdiniInFormatoLista.ElementAt(j));
+                        ordineIdPerHash = elencoIdInFormatoLista.ElementAt(i).IdOrdine;
+                    }
+                }
+                elencoIdOrdiniConAssociatoElencoProdotti.Add(ordineIdPerHash, listaTmp);
+            }
+
+            ViewData["elencoOrdini"] = elencoIdOrdiniConAssociatoElencoProdotti;
+
+            return View();
+        }
     }
 }
---
+++ GranBazar/wwwroot/js/script.js
@@ -0,0 +1,111 @@
+﻿/*********************************************
+			filtro
+*********************************************/
+
+(function () {
+    'use strict';
+    var $ = jQuery;
+    $.fn.extend({
+        filterTable: function () {
+            return this.each(function () {
+                $(this).on('keyup', function (e) {
+                    $('.filterTable_no_results').remove();
+                    var $this = $(this),
+                        search = $this.val().toLowerCase(),
+                        target = $this.attr('data-filters'),
+                        $target = $(target),
+                        $rows = $target.find('tbody tr');
+
+                    if (search == '') {
+                        $rows.show();
+                    } else {
+                        $rows.each(function () {
+                            var $this = $(this);
+                            $this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
+                        })
+                        if ($target.find('tbody tr:visible').size() === 0) {
+                            var col_count = $target.find('tr').first().find('td').size();
+                            var no_results = $('<tr class="filterTable_no_results"><td colspan="' + col_count + '">No results found</td></tr>')
+                            $target.find('tbody').append(no_results);
+                        }
+                    }
+                });
+            });
+        }
+    });
+    $('[data-action="filter"]').filterTable();
+})(jQuery);
+
+$(function () {
+    // attach table filter plugin to inputs
+    $('[data-action="filter"]').filterTable();
+
+    $('.container').on('click', '.panel-heading span.filter', function (e) {
+        var $this = $(this),
+            $panel = $this.parents('.panel');
+
+        $panel.find('.panel-body').slideToggle();
+        if ($this.css('display') != 'none') {
+            $panel.find('.panel-body input').focus();
+        }
+    });
+    $('[data-toggle="tooltip"]').tooltip();
+})
+
+
+/*********************************************
+			tree list ordini
+*********************************************/
+
+$.fn.extend({
+    treeview: function () {
+        return this.each(function () {
+            // Initialize the top levels;
+            var tree = $(this);
+
+            tree.addClass('treeview-tree');
+            tree.find('li').each(function () {
+                var stick = $(this);
+            });
+            tree.find('li').has("ul").each(function () {
+                var branch = $(this); //li with children ul
+
+                branch.prepend("<i class='tree-indicator glyphicon glyphicon-chevron-right'></i>");
+                branch.addClass('tree-branch');
+                branch.on('click', function (e) {
+                    if (this == e.target) {
+                        var icon = $(this).children('i:first');
+
+                        icon.toggleClass("glyphicon-chevron-down glyphicon-chevron-right");
+                        $(this).children().children().toggle();
+                    }
+                })
+                branch.children().children().toggle();
+
+                /**
+                 *	The following snippet of code enables the treeview to
+                 *	function when a button, indicator or anchor is clicked.
+                 *
+                 *	It also prevents the default function of an anchor and
+                 *	a button from firing.
+                 */
+                branch.children('.tree-indicator, button, a').click(function (e) {
+                    branch.click();
+
+                    e.preventDefault();
+                });
+            });
+        });
+    }
+});
+
+/**
+ *	The following snippet of code automatically converst
+ *	any '.treeview' DOM elements into a treeview component.
+ */
+$(window).on('load', function () {
+    $('.treeview').each(function () {
+        var tree = $(this);
+        tree.treeview();
+    })
+})
